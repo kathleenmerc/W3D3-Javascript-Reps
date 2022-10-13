e5969193fa9b0a2b5b5133e2aeea9353ed434a4e
Kathleen Mercado <kathleenmerc@gmail.com>
2022-10-13 14:05:25 -0400

    Get Even answered

--- javascipt-reps.js
+++ javascipt-reps.js
@@ -7,3 +7,13 @@
 for (i = 1; i <=20; i++) {
     console.log(i)
 }
+
+//2.
+// Get Even
+// Write a for loop that will log only the even numbers in 0 through 200.
+
+// Hint: Think about the increment expression.
+
+for (i=0; i <=20; i+=2){
+    console.log(i)
+}
