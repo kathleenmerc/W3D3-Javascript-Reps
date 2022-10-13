c49bc4a92443ba49305af93766150d051b2145db
Kathleen Mercado <kathleenmerc@gmail.com>
2022-10-13 14:13:32 -0400

    Fizz Buzz answered

--- javascipt-reps.js
+++ javascipt-reps.js
@@ -17,3 +17,24 @@ for (i = 1; i <=20; i++) {
 for (i=0; i <=20; i+=2){
     console.log(i)
 }
+
+//3. 
+// Fizz Buzz
+// This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
+
+// Write a javascript application that logs all numbers from 1 - 100.
+// If a number is divisible by 3 log "Fizz" instead of the number.
+// If a number is divisible by 5 log "Buzz" instead of the number.
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+
+for (i=1; i<=100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log ("FizzBuzz")
+    } else if (i % 3 === 0) {
+        console.log("Fizz")
+    } else if (i % 5 === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
